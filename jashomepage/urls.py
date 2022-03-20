dfb99603065c9f6ce57477aa11334941406b98b6
jejuairservice <gwlee@aekyung.kr>
2022-03-20 19:08:37 +0900

    1

--- jashomepage/urls.py
+++ jashomepage/urls.py
@@ -21,7 +21,7 @@ from django.views.static import serve
 #from django.urls import re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('main/', include('main.urls')),
     #re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}), # 미디어 부분 로딩
